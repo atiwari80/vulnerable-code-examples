f7736d5beeee8a399aa188963ce48f9c3296e7c0
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-07 12:53:23 -0400

    fix: remediate vulnerability in SAST/php/basic-collection.php

--- SAST/php/basic-collection.php
+++ SAST/php/basic-collection.php
@@ -1,1 +1,4 @@
-%content%
+function addElement(&$array) { // %content%
+
+    $cmd = escapeshellarg($_GET['name']); // Changed: Add escapeshellarg for command injection vulnerability prevention
+    return $array; // %content%

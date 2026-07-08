720197a654b232185b201459113508066f2e3ddc
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 16:29:10 -0400

    fix: remediate vulnerability in SAST/php/basic-collection.php

--- SAST/php/basic-collection.php
+++ SAST/php/basic-collection.php
@@ -1,19 +1,4 @@
-
-<?php
-
-// Cross-Site Scripting (XSS)
-$name = $_GET['name'];
-echo('Hello ' . $name);
-
-// SQL Injection
-$id = $_POST['id'];
-mysql_query("SELECT user FROM users WHERE id = " . $id);
-
-// Command Injection
-$cmd = $_COOKIE['cmd'];
-exec("cat /var/log/apache2/access.log | grep " . $cmd);
-
-// Deprecated Function
-$words = split(":", "split:this");
-
-?>
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$statement = $mysqli->prepare('SELECT user FROM users WHERE id = ?');
+$statement->bind_param('i', $id);
+$result = $statement->execute();

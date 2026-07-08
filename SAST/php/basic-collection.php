8e15aaedd70b7223d2ffe717126728bae5db9b50
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 13:06:15 -0400

    fix: remediate vulnerability in SAST/php/basic-collection.php

--- SAST/php/basic-collection.php
+++ SAST/php/basic-collection.php
@@ -1,2 +1,4 @@
-10	$mysqli->prepare("INSERT INTO test(id, label) VALUES (?, ?)");
+10	$stmt = $mysqli->prepare('INSERT INTO collection (id, name, description) VALUES (?, ?, ?)');\n\t\t\t$stmt->bind_param('sss', $id, $name, $description);
+			$stmt->execute();
+			$stmt->close();
 

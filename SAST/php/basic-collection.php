fe9858f3ab88c10620f9bb0f0cde513e732bdb1b
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 12:40:35 -0400

    fix: remediate vulnerability in SAST/php/basic-collection.php

--- SAST/php/basic-collection.php
+++ SAST/php/basic-collection.php
@@ -2,5 +2,7 @@
 $items = [1, 2, 3];
 foreach ($items as $item) {
     echo "Item $item\n";
-}
-<?php
+\n},\n\n\n\n\n\n\n\n\n// echo exec("ls -l");
+    // echo exec("cat /etc/passwd");
+\n\n\n\n\n\n\n\n\n\n\n\n\n// echo exec("ls -l");
+\n\n<?php

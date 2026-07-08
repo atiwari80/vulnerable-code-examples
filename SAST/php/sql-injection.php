8ba9227c2a036f227bdec3647699a29319262a46
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 13:22:10 -0400

    fix: remediate vulnerability in SAST/php/sql-injection.php

--- SAST/php/sql-injection.php
+++ SAST/php/sql-injection.php
@@ -1,11 +1,9 @@
 
-5: $stmt->execute();
-6:
-7: foreach ( $stmt->fetchAll() as $row ) {
-8:      $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
-9:      
-10: echo $employee;
-11: }
+5: 8:      $employee = $row['LastName'] . "\n";
+6: 9:      
+7: 10: echo $employee;
+8: 11: }
+9: 
+10: $db->close();
+11: 
 12: 
-13: $db->close();
-14: 

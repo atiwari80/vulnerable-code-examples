6e8fc513e84b96759bb5f16266b58437d92d8eea
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 12:44:47 -0400

    fix: remediate vulnerability in SAST/python/injection/dynamic-code-injection.py

--- SAST/python/injection/dynamic-code-injection.py
+++ SAST/python/injection/dynamic-code-injection.py
@@ -1,9 +1,1 @@
-# The following code is vulnerable to arbitrary code execution because it runs dynamic Python code based on untrusted data.
-
-from flask import request
-
-@app.route("/")
-def example():
-    operation = request.args.get("operation")
-    eval(f"product_{operation}()") # Noncompliant
-    return "OK"
+<replace_with_content_from_read_file>

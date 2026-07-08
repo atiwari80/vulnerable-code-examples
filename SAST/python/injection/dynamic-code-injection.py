e3ad3cc1949dbdb77d71d92e8be56ddf252e489c
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 13:01:23 -0400

    fix: remediate vulnerability in SAST/python/injection/dynamic-code-injection.py

--- SAST/python/injection/dynamic-code-injection.py
+++ SAST/python/injection/dynamic-code-injection.py
@@ -1,17 +1,2 @@
-            import os
-            print ('Current Working Directroy is ' + os.getcwd())
-
-
-
-
-
-
-
-
-
-            # print the current working directory of the Python interpreter.
-
-            import os
-import sys
-            current_dir = os.getcwd()
-            print (current_dir) 
+8: command = \"ls -l\"
+9: os.system(command)

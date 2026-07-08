671ff8aa0084edfd73ceba1df6e7a5a060d97c85
atiwari80 <ayush.tiwari2@mphasis.com>
2026-07-08 16:34:03 -0400

    fix: remediate vulnerability in SAST/php/basic-collection.php

--- SAST/php/basic-collection.php
+++ SAST/php/basic-collection.php
@@ -1,5 +1,2 @@
-$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-$stmt = $mysqli->prepare("SELECT * FROM users WHERE name = ?");
-$stmt->bind_param("s", $name);
-$stmt->execute(); /* you can get all results and display them using $stmt */
-$stmt->close();
+$cmd = &quot;ls -l&quot;;
+exec($cmd);
